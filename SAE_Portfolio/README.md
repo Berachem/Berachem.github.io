cd0c9bfa787f52793078f64c0ab885b4ec66fb29
Berachem <berachem.markria@gmail.com>
2022-04-13 15:31:54 +0200

    changement portfolio SAE

--- SAE_Portfolio/README.md
+++ SAE_Portfolio/README.md
@@ -2,5 +2,11 @@
 # <img src="https://cours-informatique-gratuit.fr/wp-content/uploads/2014/05/code-source.png" alt="" width="30" height="30"> Section Développeur
 https://berachem.dev/
 
+<img src="https://user-images.githubusercontent.com/61350744/156548216-bd4274ce-d1d8-42d5-b578-6f06b2fbf3b6.png" width="30%">
+
+
 # <img class="rota" src="assets/img/bitcoin.svg" alt="bitcoin" width="30" height="30"> Section Crypto
 https://berachem.dev/crypto/
+
+<img src="https://user-images.githubusercontent.com/61350744/156548265-82e86633-78fb-4d0b-99d8-17ebf146287c.png" width="30%">
+
--- SAE_Portfolio/crypto/js/scripts.js
+++ SAE_Portfolio/crypto/js/scripts.js
@@ -33,7 +33,37 @@ window.addEventListener('DOMContentLoaded', event => {
 
 });
 
+$(document).on("scroll", function(){
+    var pixels = $(document).scrollTop();
+    var pageHeight = $(document).height() - $(window).height();
+    var progress = 100 * pixels / pageHeight;
+    
+    $("div.progress").css("width", progress + "%");
+  })
+
 $(document).ready(function(){
     $("a").tooltip();
     $("span").tooltip();
+    $("img").tooltip();
 });
+
+
+//Get the button:
+mybutton = document.getElementById("goTop");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+  mybutton.style.display = "block";
+} else {
+  mybutton.style.display = "none";
+}
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+
+}
