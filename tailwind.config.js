39395d90f1aff9046628c4b20b9ec01a86468a05
Berachem Markria <berachem.markria@gmail.com>
2024-11-30 17:31:45 +0100

    Responsive OK

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 const flowbite = require("flowbite-react/tailwind");
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}', flowbite.content(),],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}', flowbite.content(), ],
   darkMode: 'class',
   theme: {
     extend: {
@@ -13,27 +13,53 @@ export default {
         'slide-up': 'slideUp 0.5s ease-out',
         fadeInDown: "fadeInDown 2s ease-in-out",
         zoomIn: "zoomIn 2s infinite",
+        "pusle-slow": "pulse 2s infinite",
       },
       keyframes: {
         fadeIn: {
-          '0%': { opacity: '0' },
-          '100%': { opacity: '1' },
+          '0%': {
+            opacity: '0'
+          },
+          '100%': {
+            opacity: '1'
+          },
         },
         slideUp: {
-          '0%': { transform: 'translateY(20px)', opacity: '0' },
-          '100%': { transform: 'translateY(0)', opacity: '1' },
+          '0%': {
+            transform: 'translateY(20px)',
+            opacity: '0'
+          },
+          '100%': {
+            transform: 'translateY(0)',
+            opacity: '1'
+          },
         },
         fadeInDown: {
-          "0%": { opacity: 0, transform: "translateY(-20px)" },
-          "100%": { opacity: 1, transform: "translateY(0)" },
+          "0%": {
+            opacity: 0,
+            transform: "translateY(-20px)"
+          },
+          "100%": {
+            opacity: 1,
+            transform: "translateY(0)"
+          },
         },
         zoomIn: {
-          "0%": { opacity: 0, transform: "scale(0.4)" },
-          "50%": { opacity: 0.5 },
-          "100%": { opacity: 0, transform: "scale(1)" },
+          "0%": {
+            opacity: 0,
+            transform: "scale(0.4)"
+          },
+          "50%": {
+            opacity: 0.5
+          },
+          "100%": {
+            opacity: 0,
+            transform: "scale(1)"
+          },
         },
+
       },
     },
   },
-  plugins: [flowbite.plugin(),],
+  plugins: [flowbite.plugin(), ],
 };
